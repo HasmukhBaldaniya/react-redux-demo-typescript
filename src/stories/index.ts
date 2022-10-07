fad4dc7af196caa213d79686792f59dc89c22525
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2022-10-07 10:55:40 +0530

    extra component added

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,7 +1,11 @@
 import Input from './components/Input/Input.component';
 import Checkbox from './components/Checkbox';
+import Table from './components/Table';
+import Pagination from './components/Pagination';
 
 export default {
     Input,
-    Checkbox
+    Checkbox,
+    Table,
+    Pagination
 }
